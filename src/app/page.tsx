b268822716c334a97002cb6573eaf8f63b104266
Camilo Perez <gumball1618@gmail.com>
2025-04-29 16:54:28 -0500

    inventario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,35 @@
-import { Tabla } from "./component/tabla/tabla";
 
-export default function Home() {
-  return (
-<div className="w-full "> <Tabla headers={['id','producto','cantidad','precio']} data={[{
-    id: '1',
-    producto: 'asd',
-    cantidad: '1',
-    precio: '123'}]}/>
-    </div>
+  'use client'
+  import React, { useEffect, useState } from 'react';
+  import axios from 'axios';
+  import { Tabla } from "./component/tabla/tabla";
 
-  );
-}
+  async function getInventario() {
+      try {
+          const response = await axios.get('http://127.0.0.1:8000/api/inventario');
+          const data = response.data.data;
+
+          return { data };
+      } catch (error) {
+          console.error(error);
+          return { data: []};
+      }
+  }
+
+  export default function Home() {
+      const [data, setData] = useState([]);
+
+      useEffect(() => {
+          const fetchData = async () => {
+              const { data} = await getInventario();
+              setData(data);
+          };
+          fetchData();
+      }, []);
+
+      return (
+          <div className="w-full">
+              <Tabla headers={['id','producto_id','tipoMovimiento','cantidad','precio']} data={data} />
+          </div>
+      );
+  }
